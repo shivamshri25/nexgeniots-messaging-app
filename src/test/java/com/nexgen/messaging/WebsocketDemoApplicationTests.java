267bb078c5e3b6453cc3b80f822637195da97616
Shivam Srivastava <ssrivastava11479@altimetrik.com>
2022-06-07 19:33:57 +0530

    initial commit

---
+++ src/test/java/com/nexgen/messaging/WebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexgen.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebsocketDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
